be07c680d9ff622e1adb036ea33113cbfd777996
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-08-05 22:46:03 +0200

    Update

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));
