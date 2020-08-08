929b409bcdee5c59e7c8de0f1776c780915971fe
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-08-08 14:25:57 +0200

    Update

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
@@ -17,3 +20,23 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+
+// User API Routes
+// 2. 1:59 Request and Body Validation
+// 2. 03:39 Express Validator
+// 2. 07:32
+// 3. 11:20
+// 4. 00:29
+// 4. 07:02
+// 5. 07:42
+// 5. 11:21
+// 6. 06:54
+//
+//Profile API Routes
+//
+//
+//
+//
+//
+//
+//
