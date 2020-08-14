6b9eba3edf8339321ab7bfe432743fe9e248ad3a
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-08-14 23:59:24 +0200

    Update

--- client/src/routes/api/profile.js
+++ client/src/routes/api/profile.js
@@ -42,6 +42,7 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+      console.log(error);
     }
   }
 );
